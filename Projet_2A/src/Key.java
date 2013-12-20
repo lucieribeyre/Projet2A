d284166b3758aec5bae87e00668bb0b9baec43ec
lucie.ribeyre <lucie.ribeyre@10.70.0.207>
2013-12-20 12:43:53 +0100

    Check de projet 2A

---
+++ Projet_2A/src/Key.java
@@ -0,0 +1,36 @@
+import java.awt.Rectangle;
+
+
+/**
+ * Black and white keys or notes on the piano.
+ */
+class Key extends Rectangle {
+	int noteState = OFF;
+	int kNum;
+	final int ON = 0, OFF = 1;
+	
+	public Key(int x, int y, int width, int height, int num) {
+		super(x, y, width, height);
+		kNum = num;
+	}
+	public boolean isNoteOn() {
+		return noteState == ON;
+	}
+	public void on() {
+		setNoteState(ON);
+		cc.channel.noteOn(kNum, cc.velocity);
+		if (record) {
+			createShortEvent(NOTEON, kNum);
+		}
+	}
+	public void off() {
+		setNoteState(OFF);
+		cc.channel.noteOff(kNum, cc.velocity);
+		if (record) {
+			createShortEvent(NOTEOFF, kNum);
+		}
+	}
+	public void setNoteState(int state) {
+		noteState = state;
+	}
+}
---
+++ Projet_2A/src/NoteAnimee.java
@@ -0,0 +1,24 @@
+
+public class NoteAnimee extends Thread {
+	/**
+	 * Cette classe a pour fonction de faire changer de couleur une touche/note du clavier graphique
+	 * lorsqu'elle reçoit le nom de la note NOTE.
+	 * L'objet créé est un thread
+	 */
+	
+	// l'attribut de cet objet est un clavier(cree par Claire). 
+	// Methode Clavier : getNote(); qui renvoie un tableau d'entiers
+
+	private Clavier c;
+	
+	public NoteAnimee(Clavier clavier){
+		c=clavier;		
+	}
+	
+	public void run(){
+		for(int i=0; i<c.getNote().length;i++){
+			
+		}
+	}
+
+}
