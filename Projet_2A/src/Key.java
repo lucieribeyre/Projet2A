3d901157c903e7f25aab9461caf0885121b4fd06
lucie.ribeyre <lucie.ribeyre@lucie>
2014-01-02 12:06:44 +0100

    ajout attributs NOTEON et NOTEOFF dans Key

--- Projet_2A/src/Key.java
+++ Projet_2A/src/Key.java
@@ -2,29 +2,37 @@ import java.awt.Rectangle;
 
 
 /**
- * Black and white keys or notes on the piano.
- */
-public class Key extends Rectangle {
-	int noteState;
-	int kNum;
-	final int ON = 0, OFF = 1;
-
-	public Key(int x, int y, int width, int height, int num) {
-		super(x, y, width, height);
-		kNum = num;
-	}
-	public boolean isNoteOn() {
-		return noteState == ON;
-	}
-	public void on() {
-		setNoteState(ON);
-		cc.channel.noteOn(kNum, cc.velocity);
-	}
-	public void off() {
-		setNoteState(OFF);
-		cc.channel.noteOff(kNum, cc.velocity);
-	}
-	public void setNoteState(int state) {
-		noteState = state;
-	}
+* Black and white keys or notes on the piano.
+*/
+class Key extends Rectangle {
+        int noteState;
+        int kNum;
+        final int ON = 0, OFF = 1;
+        final int NOTEON = 144;
+        final int NOTEOFF = 128;
+        
+        public Key(int x, int y, int width, int height, int num) {
+                super(x, y, width, height);
+                kNum = num;
+        }
+        public boolean isNoteOn() {
+                return noteState == ON;
+        }
+        public void on() {
+                setNoteState(ON);
+                cc.channel.noteOn(kNum, cc.velocity);
+                if (record) {
+                        createShortEvent(NOTEON, kNum);
+                }
+        }
+        public void off() {
+                setNoteState(OFF);
+                cc.channel.noteOff(kNum, cc.velocity);
+                if (record) {
+                        createShortEvent(NOTEOFF, kNum);
+                }
+        }
+        public void setNoteState(int state) {
+                noteState = state;
+        }
 }
