5528152b1753a9f7bc097b4b90bcb1523a66ebd9
lucie.ribeyre <lucie.ribeyre@lucie>
2014-01-11 15:19:51 +0100

    Modification des methodes isNoteOn() et on () de la classe Touche

--- Projet_2A/src/Animation/Touche.java
+++ Projet_2A/src/Animation/Touche.java
@@ -12,7 +12,6 @@ class Touche extends Rectangle {
 	final int NOTEON = 144;
 	final int NOTEOFF = 128;
 	final boolean record=true;
-	final ChannelData cc;
 
 	public Touche(int x, int y, int width, int height, int num) {
 		super(x, y, width, height);
@@ -23,17 +22,9 @@ class Touche extends Rectangle {
 	}
 	public void on() {
 		setNoteState(ON);
-		cc.channel.noteOn(numTouche, cc.velocity);
-		if (record) {
-			createShortEvent(NOTEON, numTouche);
-		}
 	}
 	public void off() {
 		setNoteState(OFF);
-		cc.channel.noteOff(numTouche, cc.velocity);
-		if (record) {
-			createShortEvent(NOTEOFF, numTouche);
-		}
 	}
 	public void setNoteState(int state) {
 		noteState = state;
